492af5d8c3987f03f6e773ebc2ec69a1b937d004
Iriam Gonzalez <igboffill@digitalprojex.com>
2024-01-28 10:51:58 -0500

    v1

--- index.js
+++ index.js
@@ -24,9 +24,14 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+app.get("/api/:param", function (req, res) {
+  let date =  isNaN(req.params.param) ? new Date(req.params.param): new Date(parseInt(req.params.param))
+  res.json(isNaN(date) ? {error:"Invalid Date"} : {unix: date.getTime(), utc:date.toUTCString()});
+});
+
 
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
+var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
